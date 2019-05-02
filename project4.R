462fdbaf76138df6a8458de60ff0048d8a3c6b1b
minli1004 <50221139+minli1004@users.noreply.github.com>
2019-05-02 18:23:26 -0500

    Update project4.R

--- project4.R
+++ project4.R
@@ -15,6 +15,56 @@ data[,"loan_status"] <- Recode(data[,"loan_status"],
 rm <- c("zip_code","emp_title", "title")
 datacl <- data[ , !(names(data) %in% rm)]
 
+
+
+
+
+# Helper Functions ==================================
+# One-Hot encoding
+PreProcessingMatrixOutput <- function(train.data, test.data){
+  # generate numerical matrix of the train/test
+  # assume train.data, test.data have the same columns
+  categorical.vars <- colnames(train.data)[which(sapply(train.data, 
+                                                        function(x) is.factor(x)))]
+  categorical.vars <- c(categorical.vars, "Mo_Sold", "Year_Sold")
+  train.matrix <- train.data[, !colnames(train.data) %in% categorical.vars, drop=FALSE]
+  test.matrix <- test.data[, !colnames(train.data) %in% categorical.vars, drop=FALSE]
+  n.train <- nrow(train.data)
+  n.test <- nrow(test.data)
+  for(var in categorical.vars){
+    mylevels <- sort(unique(train.data[, var]))
+    m <- length(mylevels)
+    tmp.train <- matrix(0, n.train, m)
+    tmp.test <- matrix(0, n.test, m)
+    col.names <- NULL
+    for(j in 1:m){
+      tmp.train[train.data[, var]==mylevels[j], j] <- 1
+      tmp.test[test.data[, var]==mylevels[j], j] <- 1
+      col.names <- c(col.names, paste(var, '_', mylevels[j], sep=''))
+    }
+    colnames(tmp.train) <- col.names
+    colnames(tmp.test) <- col.names
+    train.matrix <- cbind(train.matrix, tmp.train)
+    test.matrix <- cbind(test.matrix, tmp.test)
+  }
+  return(list(train = as.matrix(train.matrix), test = as.matrix(test.matrix)))
+} 
+
+# Function of removing unnecessary variables
+RemoveVariable <- function(data, var){
+  data <- data[, !colnames(data) %in% var]
+  return(data)
+}
+
+# Function of winsorization
+ProcessWinsorization <- function(train.var, test.var, quant){
+  threshold <- quantile(train.var, quant)
+  train.var[train.var > threshold] <- threshold
+  test.var[test.var > threshold] <- threshold
+  return(list(train.v = train.var, test.v = test.var))
+}
+                                                        
+                                                                                                          
 # prepare the train/test splits======================
 #Prepare the data
 i <- 1
@@ -48,5 +98,3 @@ testq4 <- read.csv ("LoanStats_2018Q4.csv")
 
 # write.table(mysubmission_2018Q3.txt, file=outname, row.names = FALSE, sep=",", col.names = TRUE)
 # write.table(mysubmission_2018Q4.txt, file=outname, row.names = FALSE, sep=",", col.names = TRUE)
-
-
