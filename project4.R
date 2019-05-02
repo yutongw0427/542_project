8cbdd7de1cab9ffefb11a19e8329af7ef87bfd8c
Yutong Wang <49324455+yutongw0427@users.noreply.github.com>
2019-05-02 15:53:24 -0500

    Update project4.R

--- project4.R
+++ project4.R
@@ -1,17 +1,35 @@
+installIfNeeded <- function(cliblist){
+  libsNeeded <- cliblist
+  libsNeeded <- libsNeeded[!(libsNeeded %in% installed.packages()[,"Package"])]
+  if(length(libsNeeded)>0) install.packages(libsNeeded)
+}
+installIfNeeded(c("car"))
+# prepare the Data======================
 setwd("~/Desktop/542/project/Project4")
-
 split <- read.csv("Project4_test_id.csv")
 data <- read.csv("LoanStats_2007_to_2018Q2.csv")
+library(car)
+data[,"loan_status"] <- Recode(data[,"loan_status"],
+                   "c('Fully Paid') = 0;
+                    c('Default','Charged Off') = 1"
+)
 # prepare the train/test splits======================
+#Prepare the data
 i <- 1
 testid <- split[,i]
 test <- data[testid,]
 train <- data[-testid,]
+train.y <- train$loan_status
+test.y <- test$loan_status
 
 # Pick a classification method====================
 result <- matrix(NA, ncol=3, nrow=3)
 colnames(result) <-c("m1","m2","m3")
 #********** model1 ******************* 
+library(glmnet)
+
+lr.model <- cv.glmnet(train[,-"loan_status"], train.y, family="binomial", alpha=1)
+lr.probs <- predict(lr.model, newx=dtm_test, type="response")
 #********** model2 ******************* 
 #********** model3 ******************* 
 
